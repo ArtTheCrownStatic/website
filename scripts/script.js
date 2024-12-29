fb88dd659b7296f2130708ca2e8d1842878b581b
ArtTheCrownStatic <hellbladeraiden@gmail.com>
2024-12-29 08:33:41 +0000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -11,17 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Map voice messages to specific audio files
     const audioFiles = {
-        vm1: "/assets/VMS/TheCherryChair.m4a",
-        vm2: "/assets/VMS/helena_1564.m4a",
-        vm3: "/assets/VMS/Emerald.opus",
-        vm4: "/assets/VMS/Fallen.aac",
-        vm5: "/assets/VMS/Ayu.mp3",
-        vm6: "/assets/VMS/auri.m4a",
-        vm7: "/assets/VMS/arthur.m4a",
-        vm8: "/assets/VMS/Reds.mp3",
-        vm9: "/assets/VMS/Dragoncy.aac",
-        vm10: "/assets/VMS/theicepeer.mp3",
-        vm11: "/assets/VMS/Ahana.mp3",
+        vm1: "../assets/VMS/TheCherryChair.m4a",
+        vm2: "../assets/VMS/helena_1564.m4a",
+        vm3: "../assets/VMS/Emerald.opus",
+        vm4: "../assets/VMS/Fallen.aac",
+        vm5: "../assets/VMS/Ayu.mp3",
+        vm6: "../assets/VMS/auri.m4a",
+        vm7: "../assets/VMS/arthur.m4a",
+        vm8: "../assets/VMS/Reds.mp3",
+        vm9: "../assets/VMS/Dragoncy.aac",
+        vm10: "../assets/VMS/theicepeer.mp3",
+        vm11: "../assets/VMS/Ahana.mp3",
     };
 
     let currentAudio = null;
